2682818778d2a40c6c997cec6b53d997cffe6b58
kongzhou <715536017@qq.com>
2019-05-11 17:09:21 +0800

    zzm4

--- 参考/资源类别.sql
+++
@@ -1,6 +0,0 @@
---资源类别
-create table Resource_Class (
-       id serial primary key,  --每个类别的ID
-       name varchar(100) NOT NULL,  --类别名称
-       parent integer references  Resource_Class(id) on delete cascade --当前类别的父类别
-);
