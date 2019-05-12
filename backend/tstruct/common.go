03405301beb2daec27a839a2eccee90f9f9e45e5
kongzhou <715536017@qq.com>
2019-05-12 14:55:51 +0800

    zzm7

--- backend/tstruct/common.go
+++ backend/tstruct/common.go
@@ -12,10 +12,10 @@ type CaseResultData struct {
 	Name          string //详细信息不显示
 	label         []string
 
-	OperateName      string //例如故障
-	Operatetime      string //事件操作时间
-	Operator         string //操作者
-	OperatorPosition string //操作地点
-	CaseDetail       string //操作详情
-	RName            string //操作资源对象
+	OperateName     string //例如故障
+	Operatetime     string //事件操作时间
+	Operator        string //操作者
+	OperatePosition string //操作地点
+	CaseDetail      string //操作详情
+	RName           string //操作资源对象
 }
