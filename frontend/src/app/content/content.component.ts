60413b4fa50f47cd7864ce28e09e386377f44e5b
Richard <1062111902@qq.com>
2019-05-12 17:39:17 +0800

    tenth

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -17,6 +17,7 @@ export class ContentComponent {
   ){}
 
   ngOnInit(): void {
+  
    //在初始化视图的时候，传回要查看选项的信息，得到详细信息和相关资源用于完成初始化视图
     this.http.searchContent(this.input).subscribe((data:any)=>{
       
